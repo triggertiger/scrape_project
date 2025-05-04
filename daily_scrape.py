80d0dedf5c488951d7c4e92763cb1cbe1787886d
Triggertiger <buli9876@gmail.com>
2025-05-04 16:56:06 +0200

    save daily updates to /data

--- daily_scrape.py
+++ daily_scrape.py
@@ -3,6 +3,4 @@ import os
 from core.data_handler import scrape_orchestrator, scrape_factory
 
 alpha_api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
-#scrape_orchestrator(alpha_api_key, hist=False)
-df_list = scrape_factory(alpha_api_key, hist=False)
-print(df_list)
+scrape_orchestrator(alpha_api_key, hist=False)
